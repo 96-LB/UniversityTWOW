408948d8703bedf01d9ad715ebd48a47a74432f5
Lucas LaBuff <lucasbeef00@gmail.com>
2021-06-23 03:02:43 +0000

    500 page

--- web/site.py
+++ web/site.py
@@ -12,3 +12,7 @@ def error_404(error):
 @app.errorhandler(403)
 def error_403(error):
     return render_template('403.html', error=error), 403
+
+@app.errorhandler(500)
+def error_500(error):
+    return render_template('500.html', error=error), 500
