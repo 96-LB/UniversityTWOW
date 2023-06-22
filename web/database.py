69eed871d17a7983131e9c5e5a508e3403db39e8
96-LB <lucasbeef00@gmail.com>
2023-06-22 01:34:22 +0000

    fix database problems introduced by updating packages

--- web/database.py
+++ web/database.py
@@ -1,3 +1,4 @@
+import json
 import core.data as data
 from core.web import app
 from web.permissions import requires_admin
@@ -18,12 +19,14 @@ def database(filter_type):
     except:
         return {'ERROR': 'Invalid filter type.'}
 
+    
     #outputs the database as a json object
     obj = {}
     for key in db:
         if filter_func(key):
                 obj[key] = db.get(key)
-    return obj
+    
+    return json.loads(json.dumps(obj))
 
 @app.route('/database/delete/<string:user>')
 @requires_admin
@@ -31,7 +34,7 @@ def database_delete(user):
     #removes an entry from the database
     out = {user: db.get(user)}
     data.delete(user=user)
-    return out
+    return json.loads(json.dumps(out))
 
 @app.route('/database/delete/<string:user>/<string:key>')
 @requires_admin
@@ -39,4 +42,4 @@ def database_delete_key(user, key):
     #clears a key from an entry in the database
     out = {user: db.get(user), key: data.get(key, user=user)}
     data.set(key, {}, user=user)
-    return out
+    return json.loads(json.dumps(out))
